db6ca45100bee1f64ebd620f9d68de31adeef6c6
Michael Campbell <mtcampbe@illinois.edu>
2021-01-14 08:32:28 -0600

    Fix up CI to test ALL available mechs

--- .ci/generate-test-mech.py
+++ .ci/generate-test-mech.py
@@ -5,6 +5,6 @@ import glob
 test_mechs = glob.glob("test/*.cti")
 for mech in test_mechs:
     mechname = mech[5:-4]
-    with open(f"test/{mechname}_mech.py", "w") as outf:
+    with open(f"test/mechs/{mechname}_mech.py", "w") as outf:
         code = pyro.gen_thermochem_code(ct.Solution(f"{mech}", "gas"))
         print(code, file=outf)
