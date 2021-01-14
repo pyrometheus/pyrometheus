bad984821e0b4293c2907e7ec19e18242db3fc78
Michael Campbell <mtcampbe@illinois.edu>
2021-01-14 07:02:30 -0600

    Fix up CI to test all mechs

---
+++ .ci/generate-test-mech.py
@@ -0,0 +1,9 @@
+import cantera as ct
+import pyrometheus as pyro
+import glob
+
+test_mechs = glob.glob("test/*.cti")
+for mech in test_mechs:
+    mechname = mech[5:-4]
+    with open(f"test/{mechname}_mech.py", "w") as outf:
+        outf.write(pyro.gen_thermochem_code(ct.Solution(f"{mech}", "gas")))
