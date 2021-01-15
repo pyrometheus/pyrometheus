cda03c4a45afd331642f5a650ac83c64e80f1fef
Mike Campbell <mtcampbe@illinois.edu>
2021-01-14 18:31:51 -0600

    Succinctify CI mech generator
    Co-authored-by: Andreas Klöckner <inform@tiker.net>

--- .ci/generate-test-mech.py
+++ .ci/generate-test-mech.py
@@ -2,8 +2,7 @@ import cantera as ct
 import pyrometheus as pyro
 import glob
 
-test_mechs = glob.glob("test/mechs/*.cti")
-for mech in test_mechs:
+for mech in glob.glob("test/mechs/*.cti"):
     mechname = mech[11:-4]
     with open(f"test/mechs/{mechname}_mech.py", "w") as outf:
         print(pyro.gen_thermochem_code(ct.Solution(f"{mech}", "gas")), file=outf)
