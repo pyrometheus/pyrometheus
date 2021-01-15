f9bb6b6e0c8c6327123e07b105514bbfcd2c2eb8
Mike Campbell <mtcampbe@illinois.edu>
2021-01-14 18:31:19 -0600

    Succinctify CI mech generator
    Co-authored-by: Andreas Klöckner <inform@tiker.net>

--- .ci/generate-test-mech.py
+++ .ci/generate-test-mech.py
@@ -6,5 +6,4 @@ test_mechs = glob.glob("test/mechs/*.cti")
 for mech in test_mechs:
     mechname = mech[11:-4]
     with open(f"test/mechs/{mechname}_mech.py", "w") as outf:
-        code = pyro.gen_thermochem_code(ct.Solution(f"{mech}", "gas"))
-        print(code, file=outf)
+        print(pyro.gen_thermochem_code(ct.Solution(f"{mech}", "gas")), file=outf)
