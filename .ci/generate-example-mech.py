ba8e4421487b641b8d89255a11888d63457ddec3
Michael Campbell <mtcampbe@illinois.edu>
2021-03-29 12:07:05 -0500

    Clean up example generator for CI.

--- .ci/generate-example-mech.py
+++ .ci/generate-example-mech.py
@@ -2,10 +2,10 @@ import sys
 import pyrometheus as pyro
 
 
-if __name__ == "__main__":
-    narg = len(sys.argv)
-    if narg < 3:
+narg = len(sys.argv)
+if len(sys.argv) < 3:
         print(f"Usage: {sys.argv[0]} cti_file_name mech_file_name")
+else:
     cti_file_name = sys.argv[1]
     mech_file_name = sys.argv[2]
     pyro.cti_to_mech_file(cti_file_name, mech_file_name)
