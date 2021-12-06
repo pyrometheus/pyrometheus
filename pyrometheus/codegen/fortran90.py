5d80b45d491bc89ff85eb8a60743e58292c7c5b2
Esteban Cisneros Garibay <ec1287@tigercpu.princeton.edu>
2021-12-06 16:02:22 -0500

    F90: Full template

--- pyrometheus/codegen/fortran90.py
+++ pyrometheus/codegen/fortran90.py
@@ -86,7 +86,7 @@ def wrap_code(s, indent=4):
         nspaces = count_leading_spaces(ln)
         level, remainder = divmod(nspaces, indent)
 
-        if remainder != 0:
+        if remainder != 0:            
             raise ValueError(f"indentation of '{ln}' is not a multiple of "
                     f"{indent}")
 
@@ -152,7 +152,7 @@ class FortranExpressionMapper(StringifyMapper):
                     "int(%s)" % self.rec(i, PREC_NONE)
                     for i in expr.index)
         else:
-            index_str = "int(%s)" % self.rec(expr.index, PREC_NONE)
+            index_str = "%s" % (expr.index+1)  # self.rec(expr.index, PREC_NONE)
 
         return self.parenthesize_if_needed(
                 self.format("%s(%s)",
@@ -195,9 +195,11 @@ class FortranExpressionMapper(StringifyMapper):
 
 module_tpl = Template("""
 module ${module_name}
-    implicit none
 
+    implicit none
     integer, parameter :: num_species = ${sol.n_species}
+    integer, parameter :: num_reactions = ${sol.n_reactions}
+    ${real_type}, parameter :: one_atm = ${float_to_fortran(ct.one_atm)}
     ${real_type}, parameter :: gas_constant = ${float_to_fortran(ct.gas_constant)}
     ${real_type}, parameter :: mol_weights(*) = &
         (/ ${str_np(sol.molecular_weights)} /)
@@ -205,23 +207,324 @@ module ${module_name}
         (/ ${str_np(1/sol.molecular_weights)} /)
 
 contains
-    function get_specific_gas_constant(mass_fractions)
-        ${real_type} get_specific_gas_constant
-        ${real_type} mass_fractions(num_species)
 
-        get_specific_gas_constant = gas_constant * ( &
+    subroutine get_specific_gas_constant(mass_fractions, specific_gas_constant)
+
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: specific_gas_constant
+
+        specific_gas_constant = gas_constant * ( &
                 %for i in range(sol.n_species):
                     + inv_weights(${i+1})*mass_fractions(${i+1}) &
                 %endfor
                 )
-    end
+
+    end subroutine get_specific_gas_constant
+
+    subroutine get_density(pressure, temperature, mass_fractions, density)
+
+        ${real_type}, intent(in) :: pressure
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: density
+
+        ${real_type} :: mix_mol_weight
+
+        call get_mixture_molecular_weight(mass_fractions, mix_mol_weight)
+        density = pressure * mix_mol_weight / (gas_constant * temperature)
+
+    end subroutine get_density
+
+    subroutine get_pressure(density, temperature, mass_fractions, pressure)
+
+        ${real_type}, intent(in) :: density
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: pressure
+
+        ${real_type} :: mix_mol_weight
+
+        call get_mixture_molecular_weight(mass_fractions, mix_mol_weight)
+        pressure = density * gas_constant * temperature / mix_mol_weight
+
+    end subroutine get_pressure
+
+    subroutine get_mixture_molecular_weight(mass_fractions, mix_mol_weight)
+
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: mix_mol_weight
+
+        mix_mol_weight = 1.0d0 / ( &
+                %for i in range(sol.n_species):
+                    + inv_weights(${i+1})*mass_fractions(${i+1}) &
+                %endfor
+                )
+
+    end subroutine get_mixture_molecular_weight
+
+    subroutine get_concentrations(density, mass_fractions, concentrations)
+
+        ${real_type}, intent(in) :: density
+        ${real_type}, intent(in),  dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out), dimension(num_species) :: concentrations
+
+        concentrations = density * inv_weights * mass_fractions
+
+    end subroutine get_concentrations
+
+    subroutine get_mass_averaged_property(mass_fractions, spec_property, mix_property)
+
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(in), dimension(num_species) :: spec_property
+        ${real_type}, intent(out) :: mix_property
+
+        mix_property = ( &
+            %for i in range(sol.n_species):
+                + inv_weights(${i+1})*mass_fractions(${i+1})*spec_property(${i+1}) &
+            %endfor
+        )
+
+    end subroutine get_mass_averaged_property
+
+    subroutine get_mixture_specific_heat_cp_mass(temperature, mass_fractions, cp_mix)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: cp_mix
+        
+        ${real_type}, dimension(num_species) :: cp0_r
+
+        call get_species_specific_heats_r(temperature, cp0_r)
+        call get_mass_averaged_property(mass_fractions, cp0_r, cp_mix)
+        cp_mix = cp_mix * gas_constant
+
+    end subroutine get_mixture_specific_heat_cp_mass
+
+    subroutine get_mixture_specific_heat_cv_mass(temperature, mass_fractions, cv_mix)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: cv_mix
+        
+        ${real_type}, dimension(num_species) :: cp0_r
+
+        call get_species_specific_heats_r(temperature, cp0_r)
+        cp0_r(:) = cp0_r(:) - 1.d0
+        call get_mass_averaged_property(mass_fractions, cp0_r, cv_mix)
+        cv_mix = cv_mix * gas_constant
+
+    end subroutine get_mixture_specific_heat_cv_mass
+
+    subroutine get_mixture_enthalpy_mass(temperature, mass_fractions, h_mix)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: h_mix
+        
+        ${real_type}, dimension(num_species) :: h0_rt
+
+        call get_species_enthalpies_rt(temperature, h0_rt)
+        call get_mass_averaged_property(mass_fractions, h0_rt, h_mix)
+        h_mix = h_mix * gas_constant * temperature
+
+    end subroutine get_mixture_enthalpy_mass
+
+    subroutine get_mixture_energy_mass(temperature, mass_fractions, e_mix)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: e_mix
+        
+        ${real_type}, dimension(num_species) :: h0_rt
+
+        call get_species_enthalpies_rt(temperature, h0_rt)
+        h0_rt(:) = h0_rt - 1.d0
+        call get_mass_averaged_property(mass_fractions, h0_rt, e_mix)
+        e_mix = e_mix * gas_constant * temperature
+
+    end subroutine get_mixture_energy_mass
+
+    subroutine get_species_specific_heats_r(temperature, cp0_r)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(out), dimension(num_species) :: cp0_r
+
+        %for i, sp in enumerate(sol.species()):
+        cp0_r(${i+1}) = merge(1.d0*temperature, 0.d0*temperature, &
+            temperature > 1000.d0)
+        %endfor
+
+    end subroutine get_species_specific_heats_r
+
+    subroutine get_species_enthalpies_rt(temperature, h0_rt)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(out), dimension(num_species) :: h0_rt
+
+        %for i, sp in enumerate(sol.species()):
+        h0_rt(${i+1}) = merge(1.d0*temperature, 0.d0*temperature, &
+            temperature > 1000.d0)
+        %endfor
+
+    end subroutine get_species_enthalpies_rt
+
+    subroutine get_species_entropies_r(temperature, s0_r)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(out), dimension(num_species) :: s0_r
+
+        %for i, sp in enumerate(sol.species()):
+        s0_r(${i+1}) = merge(1.d0*temperature, 0.d0*temperature, &
+            temperature > 1000.d0)
+        %endfor
+
+    end subroutine get_species_entropies_r
+
+    subroutine get_species_gibbs_rt(temperature, g0_rt)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(out), dimension(num_species) :: g0_rt
+
+        ${real_type}, dimension(num_species) :: h0_rt
+        ${real_type}, dimension(num_species) :: s0_r
+
+        call get_species_enthalpies_rt(temperature, h0_rt)
+        call get_species_entropies_r(temperature, s0_r)
+        g0_rt(:) = h0_rt(:) - s0_r(:)
+
+    end subroutine get_species_gibbs_rt
+
+    subroutine get_equilibrium_constants(temperature, k_eq)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(out), dimension(num_reactions) :: k_eq
+
+        ${real_type} :: rt
+        ${real_type} :: c0        
+        ${real_type}, dimension(num_species) :: g0_rt
+
+        rt = gas_constant * temperature
+        c0 = log(one_atm/rt)
+
+        %for i, react in enumerate(sol.reactions()):
+        %if react.reversible:
+        k_eq(${i+1}) = ${cgm(
+            ce.equilibrium_constants_expr(sol, react, Variable("g0_rt")))}
+        %else:
+        k_eq(${i+1}) = -0.1d0*temperature
+        %endif
+        %endfor
+
+    end subroutine get_equilibrium_constants
+
+    subroutine get_temperature(do_energy, enthalpy_or_energy, t_guess, mass_fractions, temperature)
+        
+        logical, intent(in) :: do_energy
+        ${real_type}, intent(in)  :: enthalpy_or_energy
+        ${real_type}, intent(in)  :: t_guess
+        ${real_type}, intent(in), dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out) :: temperature
+
+        integer :: iter
+        integer,      parameter :: num_iter = 500
+        ${real_type}, parameter :: tol = 1.0d-06
+
+        ${real_type} :: iter_temp
+        ${real_type} :: iter_energy
+        ${real_type} :: iter_energy_deriv
+        ${real_type} :: iter_rhs
+        ${real_type} :: iter_deriv
+
+        iter_rhs = 0.d0
+        iter_deriv = 1.d0
+        iter_temp = t_guess
+
+        do iter = 1, num_iter
+            if(do_energy) then
+                call get_mixture_specific_heat_cv_mass(temperature, mass_fractions, iter_energy_deriv)
+                call get_mixture_energy_mass(temperature, mass_fractions, iter_energy)
+            else
+                call get_mixture_specific_heat_cp_mass(temperature, mass_fractions, iter_energy_deriv)
+                call get_mixture_enthalpy_mass(temperature, mass_fractions, iter_energy)
+            endif
+            iter_rhs = enthalpy_or_energy - iter_energy
+            iter_deriv = (-1.d0)*iter_energy_deriv
+            iter_temp = iter_temp - iter_rhs / iter_deriv
+            if(abs(iter_rhs/iter_deriv) .gt. tol) exit
+        end do
+
+        temperature = iter_temp
+
+    end subroutine get_temperature
+
+    subroutine get_fwd_rate_coefficients(temperature, concentrations, k_fwd)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: concentrations
+        ${real_type}, intent(out), dimension(num_reactions) :: k_fwd
+
+        %for i, react in enumerate(sol.reactions()):
+        %if isinstance(react, ct.FalloffReaction):
+        k_fwd(${i+1}) = 0.d0
+        %else:
+        k_fwd(${i+1}) = ${cgm(ce.rate_coefficient_expr(react.rate, Variable("temperature")))}
+        %endif
+        %endfor
+
+        %for react in three_body_reactions:
+        k_fwd(${int(react.ID)}) = k_fwd(${int(react.ID)}) * ( &
+            ${cgm(ce.third_body_efficiencies_expr(
+            sol, react, Variable("concentrations")))})
+        %endfor
+
+    end subroutine get_fwd_rate_coefficients
+
+    subroutine get_net_rates_of_progress(temperature, concentrations, r_net)
+
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in), dimension(num_species) :: concentrations
+        ${real_type}, intent(out), dimension(num_reactions) :: r_net
+
+        ${real_type}, dimension(num_reactions) :: k_fwd
+        ${real_type}, dimension(num_reactions) :: log_k_eq
+
+        call get_fwd_rate_coefficients(temperature, concentrations, k_fwd)
+        call get_equilibrium_constants(temperature, log_k_eq)
+        %for i, react in enumerate(sol.reactions()):
+        r_net(${i+1}) = ${cgm(ce.rate_of_progress_expr(sol, react,
+                        Variable("concentrations"),
+                        Variable("k_fwd"), Variable("log_k_eq")))}
+        %endfor
+
+    end subroutine get_net_rates_of_progress
+
+    subroutine get_net_production_rates(density, temperature, mass_fractions, omega)
+
+        ${real_type}, intent(in) :: density
+        ${real_type}, intent(in) :: temperature
+        ${real_type}, intent(in),  dimension(num_species) :: mass_fractions
+        ${real_type}, intent(out), dimension(num_species) :: omega
+
+        ${real_type}, dimension(num_species)   :: concentrations
+        ${real_type}, dimension(num_reactions) :: r_net
+
+        call get_concentrations(density, mass_fractions, concentrations)
+        call get_net_rates_of_progress(temperature, concentrations, r_net)
+
+        %for i, sp in enumerate(sol.species()):
+        omega(${i+1}) = ${cgm(ce.production_rate_expr(sol, 
+            sp.name, Variable("r_net")))}
+        %endfor
+
+    end subroutine get_net_production_rates
+
 end module
 """)
 
 # }}}
 
 
-def gen_thermochem_code(sol: ct.Solution, real_type="real*8",
+def gen_thermochem_code(sol: ct.Solution, real_type="real(kind(1.d0))",
         module_name="thermochem") -> str:
     """For the mechanism given by *sol*, return Python source code for a class conforming
     to a module containing a class called ``Thermochemistry`` adhering to the
