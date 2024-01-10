d5c11590b4d73d0c1034d04842c9002a17ec2c21
Esteban <ec1287@princeton.edu>
2024-01-10 17:52:35 -0600

    Fix syntax in chem_expr module.

--- pyrometheus/chem_expr.py
+++ pyrometheus/chem_expr.py
@@ -50,8 +50,8 @@ def nasa7_conditional(t, poly, part_gen):
         part_gen(poly.coeffs[1:8], t),
         part_gen(poly.coeffs[8:15], t),
     )
-    
-    
+
+
 @singledispatch
 def poly_to_expr(poly):
     raise TypeError(f"unexpected argument type in poly_to_expr: {type(poly)}")
@@ -110,7 +110,7 @@ def _(poly: ct.NasaPoly2, arg_name):
         )
 
     return nasa7_conditional(p.Variable(arg_name), poly, gen)
-    
+
 
 @singledispatch
 def poly_deriv_to_expr(poly, arg_name):
@@ -121,12 +121,11 @@ def poly_deriv_to_expr(poly, arg_name):
 @poly_deriv_to_expr.register
 def _(poly: ct.NasaPoly2, arg_name):
     def gen(c, t):
-        assert len(c) == 7        
+        assert len(c) == 7
         return c[1] + 2 * c[2] * t + 3 * c[3] * t ** 2 + 4 * c[4] * t ** 3
 
     return nasa7_conditional(p.Variable(arg_name), poly, gen)
-        
-    
+
 
 @singledispatch
 def poly_deriv_to_enthalpy_expr(poly, arg_name):
