7f6df104476445f80bd6aa6e2f04d1cf260b9fe9
Esteban <ec1287@princeton.edu>
2024-09-21 09:29:21 -0500

    Maintain backward compat with Python 3.9

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -37,7 +37,10 @@ except ImportError:
     jnp = None
 else:
     import jax.numpy as jnp  # noqa: F401
-    jax.config.update("jax_enable_x64", True)
+    if sys.version_info.minor < 11:
+        jax.config.update("jax_enable_x64", 1)
+    else:
+        jax.config.update("jax_enable_x64", True)
     numpy_list = [np, jnp]
 
 
