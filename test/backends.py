61ca1841e9524c02d7e59e55d7b67dcd3b97da09
Esteban <ec1287@princeton.edu>
2025-03-26 10:31:26 -0500

    Cosmetic changes: usr_np -> pyro_np and ptk -> pyro_gas

--- test/backends.py
+++ test/backends.py
@@ -77,13 +77,15 @@ class PythonBackend(Backend, PythonCodeGenerator):
     def _gen_jax_wrapper(base_cls):
         class JaxPyroWrapper(base_cls):
             def _pyro_make_array(self, res_list):
-                """This works around (e.g.) numpy.exp not working with object
-                arrays of :mod:`numpy` scalars. It defaults to making object arrays,
-                however if an array consists of all scalars, it makes a "plain old"
-                :class:`numpy.ndarray`.
-
-                See ``this numpy bug <https://github.com/numpy/numpy/issues/18004>`__
+                """
+                This works around (e.g.) numpy.exp not working with
+                object arrays of :mod:`numpy` scalars. It defaults to
+                making object arrays, however if an array consists of
+                all scalars, it makes a "plain old"
+                :class:`numpy.ndarray`. See this
+                ``numpy bug <https://github.com/numpy/numpy/issues/18004>`__
                 for more context.
+
                 """
 
                 from numbers import Number
@@ -91,16 +93,16 @@ class PythonBackend(Backend, PythonCodeGenerator):
                 # arrays of shape () when handed numbers
                 all_numbers = all(
                     isinstance(e, Number)
-                    or (isinstance(e, self.usr_np.ndarray) and e.shape == ())
+                    or (isinstance(e, self.pyro_np.ndarray) and e.shape == ())
                     for e in res_list)
 
                 if all_numbers:
-                    return self.usr_np.array(
-                        res_list, dtype=self.usr_np.float64
+                    return self.pyro_np.array(
+                        res_list, dtype=self.pyro_np.float64
                     )
 
-                result = self.usr_np.empty_like(
-                    self.usr_np.array(res_list),
+                result = self.pyro_np.empty_like(
+                    self.pyro_np.array(res_list),
                 )
 
                 # 'result[:] = res_list' may look tempting, however:
@@ -111,21 +113,22 @@ class PythonBackend(Backend, PythonCodeGenerator):
                 return result
 
             def _pyro_norm(self, argument, normord):
-                """This works around numpy.linalg norm not working with scalars.
-
-                If the argument is a regular ole number, it uses :func:`numpy.abs`,
-                otherwise it uses ``usr_np.linalg.norm``.
+                """
+                This works around numpy.linalg norm not working
+                with scalars.  If the argument is a regular ole
+                number, it uses :func:`numpy.abs`, otherwise it uses
+                ``pyro_np.linalg.norm``.
                 """
                 # Wrap norm for scalars
                 from numbers import Number
                 if isinstance(argument, Number):
-                    return self.usr_np.abs(argument)
+                    return self.pyro_np.abs(argument)
                 # Needed to play nicely with Jax, which frequently creates
                 # arrays of shape () when handed numbers
-                if (isinstance(argument, self.usr_np.ndarray)
+                if (isinstance(argument, self.pyro_np.ndarray)
                         and argument.shape == ()):
-                    return self.usr_np.abs(argument)
-                return self.usr_np.linalg.norm(argument, normord)
+                    return self.pyro_np.abs(argument)
+                return self.pyro_np.linalg.norm(argument, normord)
 
         return JaxPyroWrapper
 
@@ -161,9 +164,9 @@ class Pyrometheus:
     can be interacted with as if it were the original Python implementation."""
 
     def __init__(self, mechname: str, backend: Backend, user_np):
-        """Initializes an instance of Pyrometheus for a given backend and mechanism,
-        loading the previously compiled generated code and extracting the interface
-        from it.
+        """Initializes an instance of Pyrometheus for a given backend
+        and mechanism, loading the previously compiled generated code
+        and extracting the interface from it.
 
         Parameters
         ----------
@@ -174,10 +177,13 @@ class Pyrometheus:
         user_np : module
             The numpy-like module to use for array operations within the
             generated code.
+
         """
         self.backend = backend
         self.pyro = self.backend.extract_interface(
-            importlib.import_module(f"libpyro_{self.backend.get_name()}_{mechname}"),
+            importlib.import_module(
+                f"libpyro_{self.backend.get_name()}_{mechname}"
+            ),
             mechname,
             user_np
         )
@@ -192,7 +198,7 @@ class Pyrometheus:
 
 
 def pyro_init(mechname: str, user_np,
-request: pytest.FixtureRequest) -> (ct.Solution, Pyrometheus):
+              request: pytest.FixtureRequest) -> (ct.Solution, Pyrometheus):
     """Initializes an instance of Pyrometheus for the given mechanism using the
     implementation of Pyrometheus specified by the backend option passed to
     pytest as a command line argument.
